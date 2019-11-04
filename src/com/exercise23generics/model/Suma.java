66eec04c457b6ee2b55017036aa4c65fb781012c
Cristina2194 <cristina.reyes@alumnos.udg.mx>
2019-11-03 20:49:37 -0600

    Este es un ejercicio simple para comprender el uso de generics

---
+++ src/com/exercise23generics/model/Suma.java
@@ -0,0 +1,26 @@
+package com.exercise23generics.model;
+
+public class Suma <T>
+{
+	T miObjeto;
+	
+	public Suma(T miObjeto)
+	{
+		this.miObjeto = miObjeto;
+	}
+	public T getMiObjeto()
+	{
+		return this.miObjeto;
+	}
+	
+	public static <T, T2> Double SumaNumeros(T objeto1, T2 objeto2)
+	{
+		Double objDouble1=0.0; objDouble1=0.0;
+		if(objeto1 instanceof Double && objeto2 instanceof Double)
+		{
+			objDouble1 = ((Double) objeto1).doubleValue();
+			objDouble1 = ((Double) objeto2).doubleValue();
+		}
+		return objDouble1+objDouble1;
+	}
+}
